c82dd4c227f9c0b63498b7484b65c7a1716cf947
JsonMa <mahao@iot.chinamobile.com>
2019-11-08 14:28:49 +0800

    fix: 修改解析响应payload失败bug

--- test/thing.tlv.packager.test.js
+++ test/thing.tlv.packager.test.js
@@ -264,5 +264,72 @@ describe('test/thing/tlv/packager.test.js', () => {
       assert.deepEqual(functionId, parseInt(Object.keys(params)[0]), '组合功能点解析失败');
       assert.deepEqual('string-test', params[functionId].value, '组合功能点解析失败');
     });
+
+    it('assembel register response buffer', () => {
+      const version = getRandomVersion(); // 版本号
+      const id = getRandomMsgId(true); // 消息id
+      const operations = {
+        operation: 'response',
+        type: 'device',
+        target: 'system',
+        method: 'register',
+      };
+      const functionProductId = generateFunctionId('string', 'custom', 1);
+      const functionDeviceId = generateFunctionId('string', 'custom', 2);
+      const functionToken = generateFunctionId('string', 'custom', 3);
+      const functionBrokerAddress = generateFunctionId('string', 'custom', 4);
+      const packagedBufferPadyload = app.thing.tlv.packager.package({
+        version,
+        ...id ? {
+          id,
+        } : null,
+        operations,
+        code: 0,
+        data: {
+          params: [{
+            functionId: functionProductId,
+            valueType: 'string',
+            value: 'test-pid',
+          }, {
+            functionId: functionDeviceId,
+            valueType: 'string',
+            value: 'test-device-id',
+          }, {
+            functionId: functionToken,
+            valueType: 'string',
+            value: 'test-token',
+          },
+          {
+            functionId: functionBrokerAddress,
+            valueType: 'string',
+            value: 'test-broker-address',
+          },
+          ],
+
+        },
+      }); // tlv数据封装
+
+      const {
+        version: parsedVersion,
+        id: parsedId,
+        operations: parsedOperations,
+        time,
+        data: {
+          params,
+        },
+      } = app.thing.tlv.parser.parse(packagedBufferPadyload); // tlv数据解析
+      assert(parsedVersion === version, '版本号错误');
+      assert(id === parsedId, '消息id错误');
+      assert(time && typeof time === 'number', '需包含时间戳');
+      assert.deepStrictEqual(operations, parsedOperations, 'operations解析错误');
+      assert.deepEqual(functionProductId, parseInt(Object.keys(params)[0]), '产品id功能点解析失败');
+      assert.deepEqual(functionDeviceId, parseInt(Object.keys(params)[1]), '设备sn功能点解析失败');
+      assert.deepEqual(functionToken, parseInt(Object.keys(params)[2]), 'token功能点解析失败');
+      assert.deepEqual(functionBrokerAddress, parseInt(Object.keys(params)[3]), 'broker地址功能点解析失败');
+      assert.deepEqual('test-pid', params[functionProductId].value, '产品id功能点值解析失败');
+      assert.deepEqual('test-device-id', params[functionDeviceId].value, '设备sn功能点值解析失败');
+      assert.deepEqual('test-token', params[functionToken].value, 'token功能点值解析失败');
+      assert.deepEqual('test-broker-address', params[functionBrokerAddress].value, 'broker地址功能点值解析失败');
+    });
   });
 });
